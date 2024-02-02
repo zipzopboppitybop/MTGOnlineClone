8f7917492284c53a0eda1df15f4ae61bad39019c
Brian <bw49078@gmail.com>
2024-02-02 15:09:37 -0600

    added mtg api to my own

--- app/api/card_routes.py
+++ app/api/card_routes.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, request
 from flask_login import login_required, current_user
-from app.models import Card, db, User
+from app.models import db, User
 from .auth_routes import validation_errors_to_error_messages
 from datetime import date
+from mtgsdk import Card, Set
 
 card_routes = Blueprint('cards', __name__)
 
@@ -12,18 +13,18 @@ def cards():
     """
     Query for all cards and returns them is a list of dictionaries
     """
-    cards = [card.to_dict() for card in Card.query.all()]
+    cards = [card.name for card in Card.where(page=5).where(pageSize=100).all()]
     return cards
 
 
-@card_routes.route('/<int:id>')
-def card(id):
+@card_routes.route('/<name>')
+def card(name):
     """
-    Query for a card by id and return it in a dictionary
+    Query for a card by name and return it in a dictionary
     """
-    card = Card.query.get(id)
-    if card is None:
-        return { 'errors': ['Card not found!'] }, 404
+    cards = [{"cardName": card.name, "cardId": card.id} for card in Card.where(name=name).all()]
+    if cards is None:
+        return { 'errors': ['Cards not found!'] }, 404
 
-    return card.to_dict()
+    return cards
 
