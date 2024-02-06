49c2404f04d97af43bfd3a90ce26653ee267a3ac
Brian <bw49078@gmail.com>
2024-02-06 09:09:10 -0600

    created search bar for cards

---
+++ react-app/src/components/Cards/CardSearchResults/index.js
@@ -0,0 +1,66 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { thunkGetAllDecks } from '../../../store/deck';
+import { useLocation } from 'react-router-dom/cjs/react-router-dom.min';
+import DeckItem from '../DeckItem';
+import "./DeckSearch.css"
+import MissingDeck from '../../Missing/MissingDeck';
+import DeckSearchBar from '../../SearchBar/DeckSearchBar';
+
+
+const CardSearchResults = () => {
+    // const dispatch = useDispatch()
+    // const decks = useSelector(state => state.decks.deckList);
+    // const location = useLocation()
+    // const query = new URLSearchParams(location.search).get('query').toLowerCase();
+    // const filteredDecks = [];
+    // useEffect(() => {
+    //     dispatch(thunkGetAllDecks())
+    // }, [dispatch,])
+
+    // if (decks) {
+    //     for (let deck of decks) {
+    //         let element = deck.name.toLowerCase();
+    //         if (element.includes(query)) filteredDecks.push(deck)
+    //     }
+    // }
+
+    return (
+        <>
+        <h1>hello</h1>
+            {/* {decks ? (
+                <>
+                    <h1 className='search-header'>Search Decks</h1>
+                    <DeckSearchBar query={query} />
+                    <h2 id='feed-headline'>Results: {filteredDecks.length}</h2>
+                </>
+            ) : (
+                <>
+                    <h1 className='search-header'>Search Decks</h1>
+                    <DeckSearchBar />
+                    <h2 id='feed-headline'>Results: 0</h2>
+                </>
+            )}
+
+
+            {filteredDecks.length > 0 ? (
+                <div className='feed'>
+                <ul className='decks-list'>
+                    {filteredDecks?.sort((a, b) => new Date(b?.createdAt) - new Date(a?.createdAt))?.map(deck =>
+                    (
+                        <li key={deck?.id} className="deck-item">
+                            <DeckItem deck={deck} />
+                        </li>
+                    )
+                    )
+                    }
+                </ul>
+                </div>
+            ) : (
+            <MissingDeck />
+            )} */}
+        </>
+    )
+}
+
+export default CardSearchResults;
---
+++ react-app/src/components/SearchBar/CardSearchBar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useState } from 'react';
+import './SearchBar.css';
+
+function CardSearchBar(){
+    const [query, setQuery] = useState('');
+    const handleSearch = () => {
+        window.location.href = `/search/cards/?query=${query}`;
+    };
+
+	return (
+        <>
+            <div className='main-searchbar-div'>
+                <input  
+                    className='main-searchbar' 
+                    type="text" 
+                    placeholder="Card name..."
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)}
+                    onKeyPress={(e) => {
+                        if (e.key === 'Enter') {
+                            handleSearch();
+                        }
+                    }}
+                    /> 
+                <i className="fas fa-search profile-icon" />
+            </div>
+        </>
+
+	);
+}
+
+export default CardSearchBar;
