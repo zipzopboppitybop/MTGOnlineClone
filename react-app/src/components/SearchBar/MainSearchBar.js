2244fd89a7358efbb364a58cff6caaa4a220a4fd
Brian <bw49078@gmail.com>
2024-02-06 06:13:33 -0600

    added list to main search bar so when user goes to search they can narrow it down to users,decks, or cards.

--- react-app/src/components/SearchBar/MainSearchBar.js
+++ react-app/src/components/SearchBar/MainSearchBar.js
@@ -8,25 +8,34 @@ function MainSearchBar(){
     const handleSearch = () => {
         window.location.href = `/search/decks/?query=${query}`;
     };
+    const ulClassName = "search-query-list" + (query? "" : " hidden");
 
     if ( !window.location.href.includes("/search/decks/") ) placeholder = "Search for cards, decks, and users";
 
 	return (
-        <div className='main-searchbar-div'>
-            <input  
-                className='main-searchbar' 
-                type="text" 
-                placeholder={placeholder}
-                value={query}
-                onChange={(e) => setQuery(e.target.value)}
-                onKeyPress={(e) => {
-                    if (e.key === 'Enter') {
-                        handleSearch();
-                    }
-                }}
-                 /> 
-            <i className="fas fa-search profile-icon" />
-        </div>
+        <>
+            <div className='main-searchbar-div'>
+                <input  
+                    className='main-searchbar' 
+                    type="text" 
+                    placeholder={placeholder}
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)}
+                    onKeyPress={(e) => {
+                        if (e.key === 'Enter') {
+                            handleSearch();
+                        }
+                    }}
+                    /> 
+                <i className="fas fa-search profile-icon" />
+            </div>
+            <ul className={ulClassName}>  
+                <li className='search-query-list-item border-bottom'>Search Decks: {query} </li>
+                <li className='search-query-list-item border-bottom border-top'>Search Cards: {query}</li>
+                <li className='search-query-list-item border-top'>Search Users: {query}</li>
+            </ul>
+        </>
+
 	);
 }
 
