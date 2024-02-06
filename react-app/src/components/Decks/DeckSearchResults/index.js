53e850f53f33c2d658d51d7a91112015d0488f0a
Brian <bw49078@gmail.com>
2024-02-05 22:06:26 -0600

    can now search for decks by name and return them in a list

--- react-app/src/components/Decks/DeckSearchResults/index.js
+++ react-app/src/components/Decks/DeckSearchResults/index.js
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { thunkGetAllDecks } from '../../../store/deck';
 import { useLocation } from 'react-router-dom/cjs/react-router-dom.min';
-import Loading from '../../LoadingScreen';
 import DeckItem from '../DeckItem';
 import "./DeckSearch.css"
 import MainSearchBar from '../../SearchBar/MainSearchBar';
@@ -13,12 +12,21 @@ const DeckSearchResults = () => {
     const dispatch = useDispatch()
     const decks = useSelector(state => state.decks.deckList);
     const location = useLocation()
-    const query = new URLSearchParams(location.search).get('query')
+    const query = new URLSearchParams(location.search).get('query').toLowerCase();
+    const filteredDecks = [];
     useEffect(() => {
         dispatch(thunkGetAllDecks())
     }, [dispatch,])
 
-    console.log(decks)
+    if (decks) {
+        for (let deck of decks) {
+            let element = deck.name.toLowerCase();
+            if (element.includes(query)) filteredDecks.push(deck)
+        }
+    }
+
+    console.log(filteredDecks)
+    
 
     return (
         <>
@@ -26,7 +34,7 @@ const DeckSearchResults = () => {
                 <>
                     <h1 className='search-header'>Search Decks</h1>
                     <MainSearchBar />
-                    <h4 id='feed-headline'>Results: {decks.length}</h4>
+                    <h4 id='feed-headline'>Results: {filteredDecks.length}</h4>
                 </>
             ) : (
                 <>
@@ -40,7 +48,7 @@ const DeckSearchResults = () => {
             {decks ? (
                 <div className='feed'>
                 <ul className='decks-list'>
-                    {Object?.values(decks)?.sort((a, b) => new Date(b?.createdAt) - new Date(a?.createdAt))?.map(deck =>
+                    {filteredDecks?.sort((a, b) => new Date(b?.createdAt) - new Date(a?.createdAt))?.map(deck =>
                     (
                         <li key={deck?.id} className="deck-item">
                             <DeckItem deck={deck} />
